9484ad0d3712a840a1a998137269fe21b99a44bd
Yucong Jin <yucongj@microsoft.com>
2017-11-09 22:58:49 -0800

    add example

---
+++ examples/basic/index.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+.grid-example
+  .filter-container
+  .grid-container
+    grid(config="gridConfig", grid-dataSource="gridDataSource")
+      scroll(view-port={{window}} virtualized header.sticky={{ {offset => () => {
+        // calculating offset
+      }} }})
+      filter(v-bind:conditions="filters")
+  .pagination-control-container
+</template>
+
+<script>
+import VueProjectionGrid, { ScrollingPlugin, FilterPlugin } from 'vue-projection-grid';
+
+export default {
+  components: {
+    grid: VueProjectionGrid,
+    scroll: ScrollingPlugin,
+    filter: FilterPlugin,
+  },
+  data() {
+    return {
+      gridConfig: {
+        tableClasses: ['bingadsgrid'],
+        columns: [
+          // ...columns
+        ],
+      },
+      gridDataSource: query => {
+        // ...fetching data
+      },
+    }
+  },
+  computed() {
+    filters() {
+      // get filters from external filter controls
+    }
+  }
+}
+</script>
+
+
