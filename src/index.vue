bf5598267748eb183a845f2232c63c86bcf20187
Yucong Jin <yucongj@microsoft.com>
2017-11-12 19:32:35 -0800

    add first child component

--- src/index.vue
+++ src/index.vue
@@ -1,10 +1,17 @@
 <template>
-  <h1>vue-projection-grid</h1>
+  <div>
+    <h1>vue-projection-grid</h1>
+    <child />
+  </div>
 </template>
 
 <script>
+import FirstComponent from './components/first-component.vue';
+
 export default {
-  //...
+  components: {
+    child: FirstComponent,
+  },
 }
 </script>
 
