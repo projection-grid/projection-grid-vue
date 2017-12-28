37cf52176a802097279c62a29c61a20161752444
Yucong Jin <jinyucong911@outlook.com>
2017-12-28 02:38:32 -0800

    adopt core 0.2.3

--- src/projections/default-content.js
+++ src/projections/default-content.js
@@ -1,27 +1,8 @@
 import DefaultContent from './default-content.vue';
 
-function wrap(model) {
-  const { content } = model;
-
-  return content && content.Component ? model : {
-    ...model,
-    content: {
-      Component: DefaultContent,
-      props: { text: content },
-    },
-  };
-}
-
-export default function ({
-  composeCaption,
-  composeTds,
-}) {
-  return {
-    composeCaption(caption) {
-      return wrap(composeCaption(caption));
-    },
-    composeTds(td) {
-      return composeTds(td).map(wrap);
-    },
+export default function defaultContentFactory(content) {
+  return content && content.Component ? content : {
+    Component: DefaultContent,
+    props: { text: content },
   };
 }
